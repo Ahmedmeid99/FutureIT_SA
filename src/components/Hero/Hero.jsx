bd50af0e29dc488f511ec7e4ff3c5bb5a8c62755
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2026-06-11 21:06:25 +0300

    first commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,7 +15,24 @@ const Hero = () => {
             </p>
             <div className={styles.ctaContainer}>
               <button className={styles.mainBtn}>
-                Get a Free Consultation
+                <span>Get a Free Consultation</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-arrow-right ml-2 w-5 h-5"
+                  data-loc="client/src/pages/Home.tsx:134"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="m12 5 7 7-7 7"></path>
+                </svg>
               </button>
               <button className={styles.lightBtn}>Explore Our Services</button>
             </div>
